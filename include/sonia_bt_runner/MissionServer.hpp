0db2679e5ea5270ad6b459e0e5c3a8864c74abe6
Clement <Bytecrackerr@github.com>
2025-09-24 18:46:09 -0400

    LOG-118: reverted changes

--- include/sonia_bt_runner/MissionServer.hpp
+++ include/sonia_bt_runner/MissionServer.hpp
@@ -32,7 +32,7 @@ class MissionServer : public rclcpp::Node{
         void abort();
 
         rclcpp_action::GoalResponse handleGoal(const rclcpp_action::GoalUUID& uuid, std::shared_ptr<const MissionControl::Goal> goal);
-        rclcpp_action::CancelResponse handleCancel();
+        rclcpp_action::CancelResponse handleCancel(const std::shared_ptr<GoalHandle> goalhandle);
         void handleAccept(const std::shared_ptr<GoalHandle> goal_handle);
 
         std::string name_;
