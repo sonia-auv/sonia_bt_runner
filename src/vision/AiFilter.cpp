2b26e86863a2b7c38afca7cc6a53e64568c300a9
Clement <Bytecrackerr@github.com>
2025-10-05 22:17:01 -0400

    debugging

--- src/vision/AiFilter.cpp
+++ src/vision/AiFilter.cpp
@@ -174,10 +174,12 @@ namespace vision{
 
                 // The searching object has been detected
                 RCLCPP_INFO(ros_node->get_logger(), "Class OK");
-                RCLCPP_INFO(ros_node->get_logger(), "%f >= %f && %f <= %f", msg_obj.confidence, confidence.value(), msg_obj.distance, max_depth.value());
+                RCLCPP_INFO(ros_node->get_logger(), "%f >= ", msg_obj.confidence);
+                RCLCPP_INFO(ros_node->get_logger(), "%f >= ", confidence.value());
+                RCLCPP_INFO(ros_node->get_logger(), "%f >= ", msg_obj.distance);
+                RCLCPP_INFO(ros_node->get_logger(), "%f >= ", max_depth.value());
                 if(msg_obj.confidence >= confidence.value() && msg_obj.distance <= max_depth.value())
                 {
-
                     //The detected object respect the confidence and the depth enter in the behavior tree
                     RCLCPP_INFO(ros_node->get_logger(), "Confidence and depth OK, a new object has been detected");
                     _detection_array.push_back(msg_obj);
