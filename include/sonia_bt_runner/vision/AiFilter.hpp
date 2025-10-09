8a31954935b93aebe0812c44b01eaf59013619cc
Log Tower <log.club.sonia@etsmtl.net>
2025-10-09 16:33:29 -0400

    Change the sequence of the parameter

--- include/sonia_bt_runner/vision/AiFilter.hpp
+++ include/sonia_bt_runner/vision/AiFilter.hpp
@@ -22,9 +22,9 @@ namespace vision{
                     BT::InputPort<float>("Confidence", 0.6, "Ai confidence"),
                     // BT::InputPort<int>("Buffer_size", "Number of detection to average"),
                     BT::InputPort<int>("Min_size_output", "minumum number of frames with at least one detection before sending results"),
+                    BT::InputPort<int>("Max_size_output", "Max size of the output array"),
                     BT::InputPort<int>("Max_frame_before_failling", "maximum allowed frames"),
                     BT::InputPort<float>("Max_depth", "Maximum allowed depth"),
-                    BT::InputPort<int>("Max_size_output", "Max size of the output array"),
                     BT::OutputPort<AiDetectionArray>("Detected_object_array")};
             }
 
