74e43e69039154e65c1c25ab1cae1a5de0218ff7
Sonia-Nemo <log.sonia.club@etsmtl.ca>
2025-10-13 12:35:02 -0400

    Removed the debug line

--- sonia_bt_runner/src/vision/AiFilter.cpp
+++ sonia_bt_runner/src/vision/AiFilter.cpp
@@ -167,10 +167,6 @@ namespace vision{
                 RCLCPP_INFO(ros_node->get_logger(), "Class OK");
                 RCLCPP_INFO(ros_node->get_logger(), "Detection before filter %s : dist = %f | conf = %f", msg_obj.class_name.c_str(), msg_obj.distance, msg_obj.confidence);
                 RCLCPP_INFO(ros_node->get_logger(), "Comparing %s and %s = %d", msg_obj.class_name.c_str(), _object.value().c_str(), msg_obj.class_name.compare(_object.value()));
-                RCLCPP_INFO(ros_node->get_logger(), "msg_obj.confidence : %d", msg_obj.confidence);
-                RCLCPP_INFO(ros_node->get_logger(), "confidence.value() : %d", confidence.value());
-                RCLCPP_INFO(ros_node->get_logger(), "msg_obj.distance : %d", msg_obj.distance);
-                RCLCPP_INFO(ros_node->get_logger(), "max_depth.value() : %d", max_depth.value());
                 
                 if(msg_obj.confidence >= confidence.value() && msg_obj.distance <= max_depth.value())
                 {
