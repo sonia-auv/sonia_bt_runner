b631fbfbaf842235e08598a7dd290d8f84cd14a0
Clement <Bytecrackerr@github.com>
2025-10-21 17:41:45 -0400

    LOG-149: revert to static function

--- sonia_bt_runner/include/sonia_bt_runner/SoniaNodes.hpp
+++ sonia_bt_runner/include/sonia_bt_runner/SoniaNodes.hpp
@@ -13,19 +13,19 @@ BT::NodeBuilder configBuilder(std::shared_ptr<rclcpp::Node> node);
 template <typename T>
 BT::NodeBuilder simpleBuilder(std::shared_ptr<rclcpp::Node> node);
 
-inline void registerInitNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
+static void registerInitNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
 {
     factory.registerBuilder<init::MissionSwitchStatus>("MissionSwitchStatus", simpleBuilder<init::MissionSwitchStatus>(node));
     factory.registerBuilder<init::EnableDisableDVL>("EnableDisableDVL", configBuilder<init::EnableDisableDVL>(node));
     factory.registerBuilder<init::ControlSetMode>("ControlSetMode", configBuilder<init::ControlSetMode>(node));
     factory.registerBuilder<init::Tare>("Tare", configBuilder<init::Tare>(node));     
 }
-inline void registerActuatorNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
+static void registerActuatorNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
 {
     factory.registerBuilder<actuator::Dropper>("Dropper", configBuilder<actuator::Dropper>(node));
     factory.registerBuilder<actuator::Torpedo>("Torpedo", configBuilder<actuator::Torpedo>(node)); 
 }
-inline void registerNavigationNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
+static void registerNavigationNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
 {
     factory.registerBuilder<navigation::Alignment>("Alignment", configBuilder<navigation::Alignment>(node));
     factory.registerNodeType<navigation::InitializeTrajectory>("InitializeTrajectory");
@@ -34,18 +34,18 @@ inline void registerNavigationNodes(BT::BehaviorTreeFactory &factory, std::share
     factory.registerBuilder<navigation::WaitTargetReached>("WaitTargetReached", configBuilder<navigation::WaitTargetReached>(node));
 }
 
-inline void registerVisionNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
+static void registerVisionNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
 {
     factory.registerBuilder<vision::AiActivation>("AiActivation", configBuilder<vision::AiActivation>(node));
     factory.registerBuilder<vision::AiFilter>("AiFilter", configBuilder<vision::AiFilter>(node));
 }
 
-inline void registerSensorNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
+static void registerSensorNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
 {
     factory.registerBuilder<sensors::DepthCheck>("DepthCheck", simpleBuilder<sensors::DepthCheck>(node));
 }
 
-inline void registerNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
+static void registerNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
 {
     registerInitNodes(factory, node);
     registerActuatorNodes(factory, node);
