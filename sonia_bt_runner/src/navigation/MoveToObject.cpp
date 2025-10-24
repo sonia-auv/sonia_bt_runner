22dc58855fe4a53e52f341653f7cc42b13ea1d5e
Jeremy Angers <swat.angers@gmail.com>
2025-10-23 20:20:12 -0400

    Added comments

--- sonia_bt_runner/src/navigation/MoveToObject.cpp
+++ sonia_bt_runner/src/navigation/MoveToObject.cpp
@@ -73,7 +73,8 @@ namespace navigation
 
         // We append the new pose to the trajectory
         RCLCPP_INFO(ros_node->get_logger(), "Trajectory computed!1");
-        Trajectory traj = getInput<Trajectory>("Trajectory");
+        Trajectory traj;
+        getInput<Trajectory>("Trajectory", traj);
         RCLCPP_INFO(ros_node->get_logger(), "Trajectory computed!2");
         traj.trajectory.push_back(t1);
         traj.trajectory.push_back(t2);
