07720b51c8d8ddbfc05b12da594c61a3358f8d7b
Jeremy Angers <swat.angers@gmail.com>
2026-06-19 15:26:01 -0400

    Added constant in AiActivation

--- sonia_bt_runner/include/sonia_bt_runner/vision/AiActivation.hpp
+++ sonia_bt_runner/include/sonia_bt_runner/vision/AiActivation.hpp
@@ -4,6 +4,13 @@
 #include "behaviortree_cpp/behavior_tree.h"
 #include "sonia_common_ros2/srv/ai_activation_service.hpp"
 
+#define AI_ACTIVATION_FRONT_NAME "Front"
+#define AI_ACTIVATION_FRONT_TYPE bool
+#define AI_ACTIVATION_BOTTOM_NAME "Bottom"
+#define AI_ACTIVATION_BOTTOM_TYPE bool
+#define AI_ACTIVATION_MODEL_NAME "Model"
+#define AI_ACTIVATION_MODEL_TYPE int
+
 namespace vision{
     class AiActivation : public BT::SyncActionNode
     {
@@ -14,9 +21,9 @@ namespace vision{
         static BT::PortsList providedPorts()
         {
             return {
-                BT::InputPort<bool>("Front", false, "true: ON, talse: OFF"),
-                BT::InputPort<bool>("Bottom", false, "true: ON, talse: OFF"),
-                BT::InputPort<int>("Model", 0, "model choice, default = 0")};
+                BT::InputPort<AI_ACTIVATION_FRONT_TYPE>(AI_ACTIVATION_FRONT_NAME, false, "true: ON, talse: OFF"),
+                BT::InputPort<AI_ACTIVATION_BOTTOM_TYPE>(AI_ACTIVATION_BOTTOM_NAME, false, "true: ON, talse: OFF"),
+                BT::InputPort<AI_ACTIVATION_MODEL_TYPE>(AI_ACTIVATION_MODEL_NAME, 0, "model choice, default = 0")};
         }
 
         BT::NodeStatus tick() override;
--- sonia_bt_runner/src/vision/AiActivation.cpp
+++ sonia_bt_runner/src/vision/AiActivation.cpp
@@ -9,29 +9,32 @@ namespace vision{
     BT::NodeStatus AiActivation::tick(){
         std::shared_ptr<sonia_common_ros2::srv::AiActivationService_Response> response;
         _request = std::make_shared<sonia_common_ros2::srv::AiActivationService_Request>();
-        BT::Expected<bool> front = getInput<bool>("Front");
-        BT::Expected<bool> bottom = getInput<bool>("Bottom");
-        BT::Expected<int> model = getInput<int>("Model");
+
+        auto front = getInput<AI_ACTIVATION_FRONT_TYPE>(AI_ACTIVATION_FRONT_NAME).value();
+        auto bottom = getInput<AI_ACTIVATION_BOTTOM_TYPE>(AI_ACTIVATION_BOTTOM_NAME).value();
+        auto model = getInput<AI_ACTIVATION_MODEL_TYPE>(AI_ACTIVATION_MODEL_NAME).value();
+
+        RCLCPP_INFO(_ros_node->get_logger(), "The front response is %d and The bottom response is %d", front, bottom);
 
         // We activate the selected AI on the requested camera(s)
-        if(front.value()&&bottom.value()){
+        if(front && bottom){
             _request->camera_choice=3;
-            _request->model_choice=model.value();
+            _request->model_choice=model;
             _ai_client->async_send_request(_request);
         }
-        else if(!front.value()&& bottom.value()){
+        else if(!front && bottom){
             _request->camera_choice=2;
-            _request->model_choice=model.value();
+            _request->model_choice=model;
             _ai_client->async_send_request(_request);
         }
-        else if(front.value()&& !bottom.value()){
+        else if(front && !bottom){
             _request->camera_choice=1;
-            _request->model_choice=model.value();
+            _request->model_choice=model;
             _ai_client->async_send_request(_request);
         }
-        else if(!front.value()&& !bottom.value()){
+        else if(!front && !bottom){
             _request->camera_choice=0;
-            _request->model_choice=model.value();
+            _request->model_choice=model;
             _ai_client->async_send_request(_request);
         }
         else
