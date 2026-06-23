76bf245c67cc156e1119e00512ca35d83db19c36
Log Tower <log.club.sonia@etsmtl.net>
2026-06-22 21:12:47 -0400

    Corrected same variable name My bad

--- sonia_bt_runner/src/navigation/ComputeTrajectoryBetweenDetections.cpp
+++ sonia_bt_runner/src/navigation/ComputeTrajectoryBetweenDetections.cpp
@@ -49,17 +49,17 @@ BT::NodeStatus ComputeTrajectoryBetweenDetections::tick()
                                        ))/2.0f};
 
     // We compute the angle made on the furtest detection between the sub and the nearest detection
-    float furthest_det_angle{std::asin((furthest_det_dist*std::sin(delta_alpha))/(2.0f*half_dist_betw_det))};
+    float det_angle{std::asin((furthest_det_dist*std::sin(delta_alpha))/(2.0f*half_dist_betw_det))};
     
     // We compute the travaling distance that the sub need to do to go between the two detection
     float moving_dist{std::sqrt(half_dist_betw_det*half_dist_betw_det + nearest_det_dist*nearest_det_dist
-                                - 2.0f*half_dist_betw_det*nearest_det_dist*std::cos(furthest_det_angle))};
+                                - 2.0f*half_dist_betw_det*nearest_det_dist*std::cos(det_angle))};
     
     // We compute the angle between the furtest detection and the direction that cross between the 2 detection
-    float furthest_sub_angle {std::asin((half_dist_betw_det*std::sin(furthest_det_angle))/moving_dist)};
+    float furthest_sub_angle {std::asin((half_dist_betw_det*std::sin(det_angle))/moving_dist)};
     
     // We compute the angle that the sub need to do to be perpendicular with the slalom
-    float angle_to_be_perp {M_PI_2-furthest_det_angle-furthest_sub_angle};
+    float angle_to_be_perp {M_PI_2-det_angle-furthest_sub_angle};
 
     // Pose 1: rotate the sub to aim it at half distance 
     TrajectoryPose p0{};
