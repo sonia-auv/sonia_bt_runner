aa5f8b62f2c0a8c1c5ad8bee729e1dc3f6553946
ArthurPV <arthurpontiervalois@gmail.com>
2026-06-26 19:41:28 -0400

    Rename SearchSlalom to MapSlaloms

--- sonia_bt_runner/include/sonia_bt_runner/vision/MapSlaloms.hpp
+++ sonia_bt_runner/include/sonia_bt_runner/vision/MapSlaloms.hpp
@@ -2,14 +2,14 @@
 
 #include "sonia_bt_runner/vision/AbstractAiFilter.hpp"
 
-#define SEARCH_SLALOM_SIDE "Side"
-#define SEARCH_SLALOM_SIDE_PARAMS SEARCH_SLALOM_SIDE, "The side, the sub should take for the slalom (Left or Right)"
-#define SEARCH_SLALOM_SIDE_TYPE std::string
+#define MAP_SLALOMS_SIDE "Side"
+#define MAP_SLALOMS_SIDE_PARAMS SEARCH_SLALOM_SIDE, "The side, the sub should take for the slalom (Left or Right)"
+#define MAP_SLALOMS_SIDE_TYPE std::string
 
 namespace vision {
-class SearchSlalom : public AbstractAiFilter {
+class MapSlaloms : public AbstractAiFilter {
 public:
-	SearchSlalom(const std::string &name, const BT::NodeConfig &config, std::shared_ptr<rclcpp::Node> node);
+	MapSlaloms(const std::string &name, const BT::NodeConfig &config, std::shared_ptr<rclcpp::Node> node);
 fault;
 	static BT::PortsList providedPorts()
         {
@@ -30,6 +30,6 @@ protected:
        BT::NodeStatus get_detection_status() override;
        void ai_filter_callback(const sonia_common_ros2::msg::DetectionArray &msg) override;
 private:
-       SEARCH_SLALOM_SIDE_TYPE _side;
+       MAP_SLALOMS_SIDE_TYPE _side;
 };
 }
---
+++ sonia_bt_runner/src/vision/MapSlaloms.cpp
@@ -0,0 +1,1 @@
+#include "sonia_bt_runner/vision/MapSlaloms.hpp"
