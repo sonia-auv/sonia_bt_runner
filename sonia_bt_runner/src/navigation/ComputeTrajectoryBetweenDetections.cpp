face1fcc18f1eb2b7ea037e5bba074e6bbb10b24
ArthurPV <arthurpontiervalois@gmail.com>
2026-06-27 11:48:33 -0400

    Comment out unused variables

--- sonia_bt_runner/src/navigation/ComputeTrajectoryBetweenDetections.cpp
+++ sonia_bt_runner/src/navigation/ComputeTrajectoryBetweenDetections.cpp
@@ -42,26 +42,26 @@ BT::NodeStatus ComputeTrajectoryBetweenDetections::tick()
     float rot_angle_to_aim_betw_two_det{(furthest_det_angle + nearest_det_angle)/(-2.0f)};
 
     // We compute the angle between the two detection angle
-    float delta_alpha{std::abs(furthest_det_angle - nearest_det_angle)};
+    // float delta_alpha{std::abs(furthest_det_angle - nearest_det_angle)};
 
     // We compute the half of the distance between the 2 detection
-    float half_dist_betw_det{std::sqrt(furthest_det_dist*furthest_det_dist + nearest_det_dist*nearest_det_dist 
-                                       - 2.0f*furthest_det_dist*nearest_det_dist*std::cos(
-                                           delta_alpha
-                                       ))/2.0f};
+    // float half_dist_betw_det{std::sqrt(furthest_det_dist*furthest_det_dist + nearest_det_dist*nearest_det_dist
+    //                                    - 2.0f*furthest_det_dist*nearest_det_dist*std::cos(
+    //                                        delta_alpha
+    //                                    ))/2.0f};
 
     // We compute the angle made on the furtest detection between the sub and the nearest detection
-    float furthest_angle{std::asin((nearest_det_dist*std::sin(delta_alpha))/(2.0f*half_dist_betw_det))};
+    // float furthest_angle{std::asin((nearest_det_dist*std::sin(delta_alpha))/(2.0f*half_dist_betw_det))};
     
     // We compute the travaling distance that the sub need to do to go between the two detection
-    float moving_dist{std::sqrt(half_dist_betw_det*half_dist_betw_det + furthest_det_dist*furthest_det_dist
-                                - 2.0f*half_dist_betw_det*furthest_det_dist*std::cos(furthest_angle))};
+    // float moving_dist{std::sqrt(half_dist_betw_det*half_dist_betw_det + furthest_det_dist*furthest_det_dist
+    //                             - 2.0f*half_dist_betw_det*furthest_det_dist*std::cos(furthest_angle))};
     
     // We compute the angle between the furtest detection and the direction that cross between the 2 detection
-    float furthest_sub_angle {std::asin((half_dist_betw_det*std::sin(furthest_angle))/moving_dist)};
+    // float furthest_sub_angle {std::asin((half_dist_betw_det*std::sin(furthest_angle))/moving_dist)};
     
     // We compute the angle that the sub need to do to be perpendicular with the slalom
-    float angle_to_be_perp {M_PI_2-furthest_angle-furthest_sub_angle};
+    // float angle_to_be_perp {M_PI_2-furthest_angle-furthest_sub_angle};
 
     // Pose 1: rotate the sub to aim it at half distance 
     TrajectoryPose p0{};
