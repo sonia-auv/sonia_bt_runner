35b527e490228645357fe3a51985aacbede270c2
ArthurPV <arthurpontiervalois@gmail.com>
2026-06-27 11:53:40 -0400

    Fix errors

--- sonia_bt_runner/src/Tracker.cpp
+++ sonia_bt_runner/src/Tracker.cpp
@@ -6,7 +6,7 @@ Tracker::Tracker(BT::Tree& tree, const std::shared_ptr<GoalHandle> goal)
         _feedback =  std::make_shared<MissionControl::Feedback>();
     }
 
-    void Tracker::callback(BT::Duration timestamp, const BT::TreeNode& node, BT::NodeStatus prev_status, BT::NodeStatus status) 
+    void Tracker::callback(BT::Duration, const BT::TreeNode& node, BT::NodeStatus, BT::NodeStatus status)
     {
         try{
             _feedback->status = BT::toStr(status);
