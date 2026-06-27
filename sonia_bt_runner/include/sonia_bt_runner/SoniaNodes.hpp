b014949b468ce04074eb276f3269b72f822dd1b5
ArthurPV <arthurpontiervalois@gmail.com>
2026-06-27 07:40:06 -0400

    Finish to implement MapSlaloms

--- sonia_bt_runner/include/sonia_bt_runner/SoniaNodes.hpp
+++ sonia_bt_runner/include/sonia_bt_runner/SoniaNodes.hpp
@@ -54,6 +54,7 @@ static void registerVisionNodes(BT::BehaviorTreeFactory &factory, std::shared_pt
     factory.registerBuilder<vision::AiFilter>("AiFilter", configBuilder<vision::AiFilter>(node));
     factory.registerBuilder<vision::TorpedoAiFilter>("TorpedoAiFilter", configBuilder<vision::TorpedoAiFilter>(node));
     factory.registerBuilder<vision::SearchWhiteSlalom>("SearchWhiteSlalom", configBuilder<vision::SearchWhiteSlalom>(node));
+    factory.registerBuilder<vision::MapSlaloms>("MapSlaloms", configBuilder<vision::MapSlaloms>(node));
 }
 
 [[maybe_unused]] static void registerNodes(BT::BehaviorTreeFactory &factory, std::shared_ptr<rclcpp::Node> node)
