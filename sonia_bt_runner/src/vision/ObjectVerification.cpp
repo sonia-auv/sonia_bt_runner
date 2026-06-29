db56bb1ad2d28377fb115e6b067fee62d5247b9e
ArthurPV <arthurpontiervalois@gmail.com>
2026-06-29 00:18:46 -0400

    Update path of model data

--- sonia_bt_runner/src/vision/ObjectVerification.cpp
+++ sonia_bt_runner/src/vision/ObjectVerification.cpp
@@ -19,7 +19,7 @@ static std::vector<std::string> parseConfig()
 
     std::stringstream st;
 
-    st << ws << "/src/proc_vision_ros2/models/IA2026AUV8/data.yaml";
+    st << ws << "/src/proc_vision_ros2/models/IAFRONT/data.yaml";
 
     auto configPath = st.str();
     std::vector<std::string> definedObjectsValue;
